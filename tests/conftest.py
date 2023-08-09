5164c0abc6ce4f0d17ba13a17a832602f1ceb879
Anhelina Babareka <anhielina.babareka@gmail.com>
2023-08-09 20:40:32 +0300

    added browser fixture

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from selene import browser
+
+
+@pytest.fixture(scope='function', autouse=True)
+def open_new_browser():
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.window_width = 1400
+    browser.config.window_height = 2800
+    browser.open('')
+
+    yield
+
+    browser.quit()
