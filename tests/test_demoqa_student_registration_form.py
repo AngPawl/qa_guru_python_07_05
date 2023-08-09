7135d5299734457698c13ac87e46f30a97ffbea9
Anhelina Babareka <anhielina.babareka@gmail.com>
2023-08-10 00:38:49 +0300

    changed the locator for a day

--- tests/test_demoqa_student_registration_form.py
+++ tests/test_demoqa_student_registration_form.py
@@ -21,7 +21,9 @@ def test_open():
     browser.element('[class="react-datepicker__year-select"]').click().element(
         'option[value = "2000"]'
     ).click()
-    browser.element('[aria-label="Choose Saturday, January 1st, 2000"]').click()
+    browser.element(
+        '[class="react-datepicker__day react-datepicker__day--001 react-datepicker__day--weekend"]'
+    ).click()
 
     browser.element('#subjectsInput').type('computer').press_tab()
 
